c3482a1c7f7ee17080fa26c1a5e66629d3190a34
Patel <mansijpatel321@gmail.com>
2024-04-24 13:40:05 -0400

    Solutions 1-2 for part 9

---
+++ part-09/part09-01_fastest_car/src/fastest_car.py
@@ -0,0 +1,23 @@
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+    def __str__(self):
+        return f"Car (make: {self.make}, top speed: {self.top_speed})"
+
+def fastest_car(cars: list):
+    top_speeds = []
+    for car in cars:
+        top_speeds.append(car.top_speed)
+    return(cars[top_speeds.index(max(top_speeds))].make)
+
+if __name__ == "main":
+    car1 = Car("Saab", 195)
+    car2 = Car("Lada", 110)
+    car3 = Car("Ferrari", 280)
+    car4 = Car("Trabant", 85)
+
+    cars = [car1, car2, car3, car4]
+    print(fastest_car(cars))
+
