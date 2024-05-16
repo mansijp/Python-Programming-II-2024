ba442f52f8ef1dafbfe7e4d1aa0c6ff2c60ca477
Patel <mansijpatel321@gmail.com>
2024-05-16 16:21:50 -0400

    Part 9, solution 14

---
+++ part-09/part09-14_list_helper/src/list_helper.py
@@ -0,0 +1,43 @@
+class ListHelper:
+
+    @classmethod
+    def greatest_frequency(cls, my_list:list):
+        freq = {}
+        for i in my_list:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        
+        greatest_freq = 0
+        result = 0
+        for key in freq:
+            print("freq[key]", freq[key])
+            if freq[key] > greatest_freq:
+                result = key
+                greatest_freq = freq[key]
+        
+        return(result)
+    
+    @classmethod
+    def doubles(cls, my_list:list):
+        freq = {}
+        for i in my_list:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+        
+        doubles = 0
+        for key in freq:
+            if freq[key] >= 2:
+                doubles += 1
+
+        return(doubles)
+    
+if __name__ == "main":
+    numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+    print(ListHelper.greatest_frequency(numbers))
+    print(ListHelper.doubles(numbers))
+
+
